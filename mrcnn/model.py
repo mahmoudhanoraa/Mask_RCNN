a428bfeac8dba7170e07f0e4d7d59bccda9515f6
Mahmoud Muhammad Nagy <mahmoudhanora.mh@gmail.com>
2018-11-21 19:15:07 +0200

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2106,7 +2106,7 @@ class MaskRCNN():
             from keras.engine import saving
         except ImportError:
             # Keras before 2.2 used the 'topology' namespace.
-            from keras.engine import topology as saving
+            from keras.engine import saving as saving
 
         if exclude:
             by_name = True
